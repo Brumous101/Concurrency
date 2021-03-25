0ee93b7cb42f31f6a09765cbc87f322885855ac4
Kyle Johnson <Brumous101@gmail.com>
2021-03-25 05:37:26 -0400

    Rough draft of Lab 6

--- Lab 6/Lab 6.py
+++ Lab 6/Lab 6.py
@@ -5,22 +5,34 @@
 # Lab 6 – Inter-Process Communication with Pipes
 
 import multiprocessing
+import time
+import random
+import os
+import sys
 
 def cat(filename,pipeout):
-	#function opens the file specified and loops by reads a line at a time,
-	# then writing each to the pipe specified by pipeout.
-	# once all lines have been read, the function should write None 
-	# to pipeout then terminate.
-
-def grep(pattern,pipein,pipeout):
-	#Loop
-	# read a line from pipein
-	# if the line read is None then write None to pipeout and terminate
-	# if the pattern specified in contained in the line
-	# then the line is written to pipeout.
-	# lines not containing the pattern are discarded. 
+	f = open(os.path.join(sys.path[0], filename), "r")
+	#print(f.read())
+	lines = f.readlines()
+	count = 0
+	for line in lines:
+		# count += 1
+		# print("Line{}: {}".format(count, line.strip())
+		pipeout.send(line.strip())
+	pipeout.send("NONE")
+	pipeout.close()
+	
+def grep(pattern,pipein, pipeout):
+	data=pipein.recv()
+	while data != "NONE":
+		data=pipein.recv()
+		#print("Message Received: "+data)
+		if pattern in data:
+			pipeout.send(data)
+	pipeout.send("NONE")
+	pipeout.close()
     
-def wc(pipein,pipeout):
+#def wc(pipein,pipeout):
  	#initialize a count variable to zero
 	#Loop
 	#read a line from pipein
@@ -30,21 +42,26 @@ def wc(pipein,pipeout):
 	#write None to pipeout and terminate
 	
 def printer(pipein):
-	#Loop
-	# read a line
-	# if the line is None then terminate
-	# otherwise print the line out using print.
+	data=pipein.recv()
+	while data != "NONE":
+		data=pipein.recv()
+		print("Message Received: "+data)
+	print("Finished")
             
-if __name__=="__main__":
-    #create pipes
-
-    #create processes
-
-    #start processes
-
-    #join processes
-
-    #enter to exit
-
-    #cat("sonnets.txt",p1write)
-    #cat("thee",p1read)
+if __name__ == '__main__':
+	pipeCon1,pipeCon2=multiprocessing.Pipe()
+	pipeCon3,pipeCon4=multiprocessing.Pipe()
+	p1=multiprocessing.Process(target=cat,args=("sonnets.txt",pipeCon1,))
+	p2=multiprocessing.Process(target=grep,args=("THEE",pipeCon2,pipeCon2,))
+	#p3=multiprocessing.Process(target=wc,args=(pipeCon2,pipeCon3,))
+	p4=multiprocessing.Process(target=printer,args=(pipeCon3,))
+	p1.start()
+	p2.start()
+	#p3.start()
+	p4.start()
+	p1.join()
+	p2.join()
+	#p3.join()
+	p4.join()
+	print("done")
+	input("enter to exit")
